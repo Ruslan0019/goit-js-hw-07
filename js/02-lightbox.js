9b2069366781bdab5af18b9b4c8a12ba17299f09
Ruslan0019 <133685041+Ruslan0019@users.noreply.github.com>
2023-10-07 23:29:11 +0300

    2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,27 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery')
+const murkup = galleryItems.map(({ preview, original, description }) =>
+    `
+    <li class="gallery__item">
+     <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+     </a>
+    </li>
+     `
+).join('');
+gallery.insertAdjacentHTML('afterbegin', murkup)
+
+
+  const gallerySimpleLightbox = new SimpleLightbox('.gallery a', {
+      captions: true, 
+      captionDelay: 250, 
+      captionSelector: 'img', 
+      captionsData: 'alt',
+      captionPosition: 'outside',
+      scrollZoom: false,
+      captionClass: 'custom-caption'
+      
+  });
